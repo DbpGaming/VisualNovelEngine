29d3cb626ca825535df8d164ded6c7db76d58577
DbpGaming <LbpDbpGaming@gmail.com>
2020-10-29 15:09:26 -0500

    half fixed i guess, and better error handling

--- VNEngine.c
+++ VNEngine.c
@@ -18,12 +18,15 @@ float messageX = 0.5;
 float messageY = 0.9;
 TTF_Font* Font;
 short scriptNum = 1;
-char config[16] = "config";
-char fontName[32];
-char scripts[16];
-char windowTitle[64];
-char images[16];
-char text[16];
+char config[] = "config";
+char fontName[255];
+char scripts[255];
+char windowTitle[255];
+char images[255];
+char text[255];
+char fileError[] = "Error opening file:";
+char memoryError[] = "Error with memory:";
+char readError[] = "Error reading file:";
 
 void renderimage(SDL_Surface* Surface, float y, float x, float w, float h)
 {
@@ -37,27 +40,26 @@ void renderimage(SDL_Surface* Surface, float y, float x, float w, float h)
 	SDL_RenderCopy(renderer, Texture, NULL, &position);
 }
 
-void error(char* message, int code)
+void error(char* message, char* file, int code)
 {
-		fputs(message,stderr); 
+		fputs(strcat(message, file),stderr); 
 		exit(code);
 }
 
-void readLine(char* out, char fileName[], int LN)
+char* readLine(char fileName[], int LN)
 {
-	printf(fileName);
 	FILE* file = fopen(fileName, "r");
 	if (file==NULL)
-		error("Error opening file (read)", 1);
+		error(fileError, fileName, 1);
 	for(int i = 0; i < LN; i++)
 	{
 		fgets(line, sizeof(line), file);
 		if (line==NULL)
-			error("Memory related error", 2);
+			error(memoryError, fileName, 2);
 		if (line[strlen(line)-1] == '\n')
 			line[strlen(line)-1] = '\0';
-		printf("Data from the file:%s\n", line);
 	}
+	printf("Data from the file:%s\n", line);
 	for (int i = 0; i < strlen(line); i++)
 	{
 		if (line[i-1] == 0x5C && line[i] == 0x6e)
@@ -67,35 +69,25 @@ void readLine(char* out, char fileName[], int LN)
 		}
 	}
 	fclose(file);
-	printf(line);
-	out = line;
-	printf(out);
+	return line;
 }
-
-//char* readLine(char fileName[255], int LN){
-//	char line;
-//	readLine(&line, fileName, LN);
-//	return &line;
-//}
 
 void openScript(int scriptID, char* &buffer)
 {
-	char script[32];
-	printf(scripts);
-	printf("3");
-	readLine(script, scripts, scriptID);
+	char* script;
+	script = readLine(scripts, scriptID);
 	FILE* file = fopen(script,"rb");
 	if (file==NULL)
-		error("Error opening file (script)", 1);
+		error(fileError, script, 1);
 	fseek (file , 0 , SEEK_END);
 	lSize = ftell (file);
 	rewind (file);
 	buffer = (char*) malloc (sizeof(char)*lSize);
 	if (buffer==NULL)
-		error("Memory related error", 2);
+		error(memoryError, script, 2);
 	result = fread (buffer,1,lSize,file);
 	if (result != lSize)
-		error("Reading error", 3);
+		error(readError, script, 3);
 	fclose (file);
 }
 
@@ -113,16 +105,12 @@ SDL_Surface* TextSurface(TTF_Font* Font, char* Text, char b, char g, char r, int
 void initValues(){
 	Resolution.w = 1280;
 	Resolution.h = 720;
-	printf("2");
-	printf(config);
 
-	readLine(config, config, 1);
-	readLine(fontName, config, 2);
-	readLine(windowTitle, config, 3);
-	readLine(scripts, config, 4);
-	printf(scripts);
-	readLine(images, config, 5);
-	readLine(text, config, 6);
+	strcpy(fontName, readLine(config, 2));
+	strcpy(windowTitle, readLine(config, 3));
+	strcpy(scripts, readLine(config, 4));
+	strcpy(images, readLine(config, 5));
+	strcpy(text, readLine(config, 6));
 }
 
 void init()
@@ -138,13 +126,11 @@ void init()
 
 int main(int argc, char *argv[])
 {
-	printf(config);
 	initValues();
 	init();
 
 	openScript(1, script);
 	activeScript = script;
-  
 	char r = 1;
 	for (int i = 0; i < lSize;)
 	{
@@ -152,7 +138,9 @@ int main(int argc, char *argv[])
 			switch(readComm(i))
 			{
 				case 0x00:
-					//renderimage(TextSurface(Font, readLine(readLine(text, scriptNum), readComm(i)), 255, 255, 255, 720), 200, 128, 255, 255);
+					//char filename[255];
+					//strcpy(filename, readLine(readLine(text, scriptNum), readComm(i)));
+					//renderimage(TextSurface(Font, testing, 255, 255, 255, 720), 200, 128, 255, 255);
 					r = 0;
 				break;
 
@@ -162,7 +150,9 @@ int main(int argc, char *argv[])
 				break;
 
 				case 0x02:
-					//renderimage(IMG_Load(readLine(scripts, readComm(i))), 128, 128, 128, 128);
+					//char filename[255];
+					//readLine(images, readComm(i)));
+					//renderimage(IMG_Load(filename), 128, 128, 128, 128);
 				break;
 
 				case 0xFE:
